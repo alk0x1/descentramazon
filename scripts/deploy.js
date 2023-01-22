4912356b8c90dc5e77d615b090a3aa9746ba3316
alk0051 <alanbertani051@gmail.com>
2023-01-21 22:20:19 -0300

    finish deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -12,7 +12,32 @@ const tokens = (n) => {
 }
 
 async function main() {
+  // setup accounts
+  const [deployer] = await hre.ethers.getSigners();
 
+  // deploy
+  const Descentramazon = await hre.ethers.getContractFactory("Descentramazon");
+  const descentramazon = await Descentramazon.deploy();
+  await descentramazon.deployed();
+
+  console.log(`Deployed Descentramazon Contract at: ${descentramazon.address}\n`);
+
+   // Listing items...
+   for (let i = 0; i < items.length; i++) {
+    const transaction = await dappazon.connect(deployer).list(
+      items[i].id,
+      items[i].name,
+      items[i].category,
+      items[i].image,
+      tokens(items[i].price),
+      items[i].rating,
+      items[i].stock,
+    )
+
+    await transaction.wait()
+
+    console.log(`Listed item ${items[i].id}: ${items[i].name}`)
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
